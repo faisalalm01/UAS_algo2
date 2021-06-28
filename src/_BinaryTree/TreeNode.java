8f4a613a12a7b3e8171a7d51096192cbcef22328
faisalalm01 <faisalali2858@gmail.com>
2021-06-29 00:12:02 +0700

    UAS sem2

---
+++ src/_BinaryTree/TreeNode.java
@@ -0,0 +1,10 @@
+package _BinaryTree;
+public class TreeNode {
+    char data;
+    TreeNode left;
+    TreeNode right;
+    
+    public TreeNode(int data) {
+        this.data=(char) data;
+    }
+}
